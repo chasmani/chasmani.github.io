64160f5afc8cba265cfa7f748370cabe0488c44b
chasmani <pilgrimcharlie2@gmail.com>
2021-04-30 12:24:15 +0100

    Added new link

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+var warwickResponseId = urlParams.get('Warwick_ResponseID');
+
+// Create anchor element.
+var a = document.createElement('a'); 
+                  
+// Create the text node for anchor element.
+var link = document.createTextNode("Click here to begin the experiment");
+                  
+// Append the text node to anchor element.
+a.appendChild(link); 
+                  
+// Set the title.
+a.title = "This is Link"; 
+                  
+var newLink = "https://pdrep.herokuapp.com/room/pd_replay/?Warwick_ResponseID=" + warwickResponseId
+
+// Set the href property.
+a.href = newLink; 
+                  
+// Append the anchor element to the body.
+document.body.appendChild(a); 
